9d7d4cad0a066749ff2e8a0e51d822dc78ce8c51
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-09 00:41:58 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -12,7 +12,6 @@ class PowerLevel(commands.Cog):
     @commands.command()
     async def powerlevel(self, ctx, *, member: discord.Member = None):
         await ctx.message.add_reaction('✅')
-        await ctx.change_nickname('Test')
          
         
 def setup(bot):
