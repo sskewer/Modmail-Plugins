5678f734841239db36cf9468c791faf26f7f4f7a
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-08 21:40:02 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -1,16 +1,27 @@
+import discord
+from discord import Embed, Guild, Member, Role
 from discord.ext import commands
+from discord.ext.commands import Bot, Cog, Context, Greedy, group
+from discord.utils import get
 
-class MyCog(commands.Cog):
+class PowerLevel(commands.Cog):
+    
     def __init__(self, bot):
         self.bot = bot
+        self.db = bot.plugin_db.get_partition(self)
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        print(message.content)
+    @group(name="supportrole", invoke_without_command=True)
+    @commands.has_any_role(659513332218331155, 676408167063879715)
+    async def supportrole(self, ctx: Context) -> None:
+        await ctx.send_help(ctx.command)
 
-    @commands.command()
-    async def say(self, ctx, *, message):
-        await ctx.send(message)
+    @supportrole.command(name="give")
+    @commands.has_any_role(659513332218331155, 676408167063879715)
+    async def givesupport(self, ctx: Context, user: discord.Member):
+        role = get(user.guild.roles, id=683333884871573534)
+        await user.add_roles(role)
+        await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving <@&{role.id}> role")
 
+        
 def setup(bot):
-    bot.add_cog(MyCog(bot))
+    bot.add_cog(PowerLevel(bot))
