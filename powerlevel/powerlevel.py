12c627caff89fcfeb53fba0e3737902b17572c69
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-08 23:18:21 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -10,12 +10,14 @@ class PowerLevel(commands.Cog):
         self.bot = bot
         
     @commands.command()
-    async def powerlevel(*args):
-        output = ''
-        for word in args:
-            output += word
-            output += ' '
-        await ctx.send(output)
+    async def powerlevel(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Hello {0.name}~'.format(member))
+        else:
+            await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
+        self._last_member = member
         
         
 def setup(bot):
