86f100605f5b99f46e2e17018e7a1ecedb59332c
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-08 22:52:53 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -4,19 +4,18 @@ from discord.ext import commands
 from discord.ext.commands import Bot, Cog, Context, Greedy, group
 from discord.utils import get
 
-class PowerLevel(commands.Cog):
     
+class PowerLevel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = bot.plugin_db.get_partition(self)
-        
-@client.command()
-async def powerlevel(*args):
-    output = ''
-    for word in args:
-        output += word
-        output += ' '
-    await client.say(output)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        print(message.content)
+
+    @commands.command()
+    async def say(self, ctx, *, message):
+        await ctx.send(message)
         
 def setup(bot):
     bot.add_cog(PowerLevel(bot))
