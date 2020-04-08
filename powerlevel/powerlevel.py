1b326ba8ba7c293d1da9a97c9e9389e76015d362
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-09 00:38:28 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -11,8 +11,7 @@ class PowerLevel(commands.Cog):
         
     @commands.command()
     async def powerlevel(self, ctx, *, member: discord.Member = None):
-        if (ctx.message.channel.id == '603955376286728226'):
-            await ctx.message.add_reaction('✅')
+        await ctx.message.add_reaction('✅')
          
         
 def setup(bot):
