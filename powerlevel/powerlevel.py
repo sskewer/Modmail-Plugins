4c82bffcf095de779eff14232cedb484f6579411
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-08 21:48:11 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -9,18 +9,11 @@ class PowerLevel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
-
-    @group(name="supportrole", invoke_without_command=True)
-    @commands.has_any_role(659513332218331155, 676408167063879715)
-    async def supportrole(self, ctx: Context) -> None:
+        
+    @group(name="powerlevel", invoke_without_command=True)
+    @commands.has_any_role(454261607799717888)
+    async def powerlevel(self, ctx: Context) -> None:
         await ctx.send_help(ctx.command)
-
-    @supportrole.command(name="give")
-    @commands.has_any_role(659513332218331155, 676408167063879715)
-    async def givesupport(self, ctx: Context, user: discord.Member):
-        role = get(user.guild.roles, id=683333884871573534)
-        await user.add_roles(role)
-        await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving <@&{role.id}> role")
 
         
 def setup(bot):
