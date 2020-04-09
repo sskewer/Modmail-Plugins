740dfe6ed8f9734227d96aa7b63963add8a4145e
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-09 11:36:27 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -4,16 +4,24 @@ from discord.ext import commands
 from discord.ext.commands import Bot, Cog, Context, Greedy, group
 from discord.utils import get
 
-    
+levels = {'1': ' [:zap: 1]',
+          '2': ' [:zap: 2]',
+          '3': ' [:zap: 3]',
+          '4': ' [:zap: 4]',
+          '5': ' [:zap: 5]'}
+
+    await ctx.author.edit(nick=ctx.author.display_name + levels[content])
+
+
 class PowerLevel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+      
     @commands.command()
     async def powerlevel(self, ctx, *, content:str):
         if content.isdigit():
            await ctx.message.add_reaction('✅')
-           await ctx.change_nickname(ctx.message.author, content)
+           await ctx.author.edit(nick=ctx.author.display_name + levels[content])
         else:
            await ctx.send(':warning: **Per favore inserisci solo numeri.**')
          
