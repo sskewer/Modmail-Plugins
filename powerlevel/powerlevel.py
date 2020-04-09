6dffe2b7fd7b061edd97ded131981b87670824a4
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-09 22:24:16 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -6,7 +6,7 @@ from discord.utils import get
 levels = {};
 def setup(maxLevel):
   for i in range(1, maxLevel+1):
-    levels[i] = " [⚡" + str(i) + "]";
+    levels[i] = " [:zap:" + str(i) + "]";
 
 # Setup
 setup(140)
@@ -21,29 +21,33 @@ class PowerLevel(commands.Cog):
         guild   = ctx.message.guild;
         author  = ctx.message.author;
         user_id = author.id;
-        error   = '**<@' + str(user_id) + '>, per favore inserisci un power level valido.**';
-        if content.isdigit():
-            # Vars
-            index  = int(content);
-            member  = guild.get_member(user_id)
-            # New Nickname
-            if index > 0 and index <= 140:
-                tag = levels[index];
-                # New Nickname
-                await member.edit(nick=author.name + ' ' + tag)
-                # Reaction
-                await ctx.message.add_reaction('✅')
-            else:
-                await ctx.send(error)
-        elif content == 'reset':
-            member  = guild.get_member(user_id)
-            # New Nickname
-            await member.edit(nick=author.name)
-            # Reaction
-            await ctx.message.add_reaction('✅')
-        else:
-           await ctx.send(error)
-         
+        channel = ctx.channel;
+        # Check
+        if channel.id == 454274882688122880 or channel.id == 454274841047072768:
+          if content.isdigit():
+              # Vars
+              index  = int(content);
+              member  = guild.get_member(user_id)
+              # New Nickname
+              if index > 0 and index <= 140:
+                  tag = levels[index];
+                  # New Nickname
+                  await member.edit(nick=author.name + ' ' + tag)
+                  # Reaction
+                  await ctx.message.add_reaction(':white_check_mark:')
+              else:
+                  # Error Message
+                  error   = '**<@' + str(user_id) + '>, per favore inserisci un power level valido.**';
+                  # Send Message
+                  await ctx.send(error)
+          elif content == 'reset':
+              member  = guild.get_member(user_id)
+              # New Nickname
+              await member.edit(nick=author.name)
+              # Reaction
+              await ctx.message.add_reaction(':white_check_mark:')
+          else:
+            await ctx.send(error)
         
 def setup(bot):
     bot.add_cog(PowerLevel(bot))
