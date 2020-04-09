cdd687209c5983424e079128149fc3c93106d1c4
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-09 12:11:54 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -1,24 +1,37 @@
-import discord
 from discord import Embed, Guild, Member, Role
 from discord.ext import commands
 from discord.ext.commands import Bot, Cog, Context, Greedy, group
 from discord.utils import get
 
-levels = {'1': ' [:zap: 1]',
-          '2': ' [:zap: 2]',
-          '3': ' [:zap: 3]',
-          '4': ' [:zap: 4]',
-          '5': ' [:zap: 5]'}
+maxLevel = 140;
+levels = {};
+
+def setup(maxLevel):
+  for i in range(1, maxLevel+1):
+    levels[i] = " [:zap: " + str(i) + "]";
+
+# Setup
+setup(maxLevel);
+
 
 class PowerLevel(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
       
     @commands.command()
-    async def powerlevel(self, ctx, *, content:str):
+    async def powerlevel(self, ctx, *, content:int):
         if content.isdigit():
-           await ctx.message.add_reaction('✅')
-           await ctx.author.edit(nick=ctx.author.display_name + levels[content])
+            # Var Definition
+            author  = ctx.author;
+            message = ctx.message;
+            # New Nickname
+            if content > 0 and content <= len(levels):
+                tag = levels[content];
+                await author.edit(nick=author.name + tag)
+                # Reaction
+                await message.add_reaction(':white_check_mark:')
+            else:
+                await ctx.send(':warning: **Per favore inserisci un power level valido.**')
         else:
            await ctx.send(':warning: **Per favore inserisci solo numeri.**')
          
