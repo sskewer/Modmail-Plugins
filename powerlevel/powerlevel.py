4e90699430b1949ab9c88d42df23200608126735
Skewer <57370513+sskewer@users.noreply.github.com>
2020-04-09 11:12:10 +0200

    Update powerlevel.py

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -13,7 +13,7 @@ class PowerLevel(commands.Cog):
     async def powerlevel(self, ctx, *, content:str):
         if content.isdigit():
            await ctx.message.add_reaction('✅')
-           nick = ctx.member.display_name
+           nick = ctx.author
            await ctx.send(nick)
         else:
            await ctx.send(':warning: **Per favore inserisci solo numeri.**')
