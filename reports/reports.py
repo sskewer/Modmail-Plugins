d020eeb7c1d8fd56f17ca7f1581f0cb4d5bafdc9
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-13 15:05:50 +0200

    Update reports.py

--- reports/reports.py
+++ reports/reports.py
@@ -56,7 +56,7 @@ class Report(commands.Cog):
         embed.add_field(name="Motivazione", value=reason, inline=False)
 
         await setchannel.send(report_mention, embed=embed)
-        await ctx.delete()
+        await ctx.message.delete()
         await ctx.author.send(f"Hai segnalato con successo **{user.name}** con la seguente motivazione: `{reason}`. Grazie per la collaborazione!")
         
 def setup(bot):
