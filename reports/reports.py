d1d519ffbcfcf66ef9610b26ee3b29477b92ec19
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-13 14:48:19 +0200

    Update reports.py

--- reports/reports.py
+++ reports/reports.py
@@ -7,7 +7,7 @@ from core import checks
 from core.models import PermissionLevel
 
 class Report(commands.Cog): 
-    """An easy way for your members to report bad behavior"""
+    """Un semplice modo per segnalare gli utenti con un comportamento scorretto"""
     
     def __init__(self, bot):
         self.bot = bot
@@ -16,28 +16,28 @@ class Report(commands.Cog):
     @commands.command(aliases=["rchannel"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def reportchannel(self, ctx, channel: discord.TextChannel):
-        """Set the reports channel"""
+        """Impostare il canale delle segnalazioni"""
         await self.db.find_one_and_update({"_id": "config"}, {"$set": {"report_channel": channel.id}}, upsert=True)
         
         embed = discord.Embed(color=discord.Color.blue(), timestamp=datetime.datetime.utcnow())
-        embed.add_field(name="Set Channel", value=f"Successfully set the reports channel to {channel.mention}", inline=False)
+        embed.add_field(name="Set Channel", value=f"Canale delle segnalazioni correttamente impostato su {channel.mention}", inline=False)
         
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["rmention"])
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def reportmention(self, ctx, *, mention: str):
-        """Sets the report mention"""
+        """Impostare le menzioni delle segnalazioni"""
         await self.db.find_one_and_update({"_id": "config"}, {"$set": {"report_mention": mention}}, upsert=True)
         
         embed = discord.Embed(color=discord.Color.blue(), timestamp=datetime.datetime.utcnow())
-        embed.add_field(name="Changed Mention", value=f"Successfully changed the report mention to {mention}", inline=False)
+        embed.add_field(name="Changed Mention", value=f"Menzioni delle segnalazioni correttamente impostato su {mention}", inline=False)
         
         await ctx.send(embed=embed)
 
     @commands.command()
     async def report(self, ctx, user: discord.Member, *, reason: str):
-        """Report member's bad behavior"""
+        """Segnalare il comportamento scorretto di un utente"""
         config = await self.db.find_one({"_id": "config"})
         report_channel = config["report_channel"]
         setchannel = discord.utils.get(ctx.guild.channels, id=int(report_channel))
@@ -50,13 +50,14 @@ class Report(commands.Cog):
         embed = discord.Embed(color=discord.Color.red(), timestamp=datetime.datetime.utcnow())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
         
-        embed.add_field(name="Reported user", value=f"{user.mention} | ID: {user.id}", inline=False)
-        embed.add_field(name="Reported by:", value=f"{ctx.author.mention} | ID: {ctx.author.id}", inline=False)
-        embed.add_field(name="Channel", value=ctx.channel.mention, inline=False)
-        embed.add_field(name="Reason", value=reason, inline=False)
+        embed.add_field(name="Utente segnalato", value=f"{user.mention} | ID: {user.id}", inline=False)
+        embed.add_field(name="Segnalato da:", value=f"{ctx.author.mention} | ID: {ctx.author.id}", inline=False)
+        embed.add_field(name="Canale", value=ctx.channel.mention, inline=False)
+        embed.add_field(name="Motivazione", value=reason, inline=False)
 
         await setchannel.send(report_mention, embed=embed)
-        await ctx.send("Succesfully reported the user!")
-                        
+        await ctx.author.send(f"Hai segnalato con successo **{user.name}** con la seguente motivazione: `{reason}`. Grazie per la collaborazione!")
+        await ctx.message.delete()
+        
 def setup(bot):
     bot.add_cog(Report(bot))
