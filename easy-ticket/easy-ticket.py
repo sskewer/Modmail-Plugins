297c69901b12306d9893a74ef40ab1af6d52f81c
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-13 17:47:26 +0200

    Update easy-ticket.py

--- easy-ticket/easy-ticket.py
+++ easy-ticket/easy-ticket.py
@@ -22,18 +22,18 @@ class TicketManagement(commands.Cog):
     async def open(self, ctx: Context, user: discord.Member):
         """Aprire un ticket per l'utente specificato"""
         # Embed
-        embed = discord.Embed(title="**Richiesta Accettata**", description=f"Hey <@{user.id}>, la tua richiesta è stata accettata e per questo abbiamo aperto un ticket. Un membero del team Vindertech ti risponderà il prima possibile in questa chat.", color=discord.Color.green(), timestamp=datetime.datetime.utcnow())
+        embed = discord.Embed(title="**Richiesta Accettata**", description=f"Hey <@{user.id}>, la tua richiesta è stata accettata e per questo abbiamo aperto un ticket. Un membero del team Vindertech ti risponderà il prima possibile.", color=discord.Color.green(), timestamp=datetime.datetime.utcnow())
         embed.set_author(name=user.name, icon_url=user.avatar_url)
         embed.set_footer(text=ctx.guild.name, icon_url=ctx.guild.icon_url)
         embed.add_field(name="**F.A.Q.**", value=f"Se hai bisogno delle domande e risposte frequenti, [clicca qui](https://www.epicgames.com/help/it/fortnite-c75).", inline=False)
-        embed.add_field(name="**Supporto Tecnico", value=f"Se hai bisogno di aiuto in gioco, contatta l'assistenza [cliccando qui](https://www.epicgames.com/help/it/contact-us?metadata=eyJoaXN0b3J5TGlua3MiOlt7InVybCI6Ii9mb3J0bml0ZS1jNzUiLCJ0aXRsZSI6IkZvcnRuaXRlIn1dLCJjYXRlZ29yeUlkIjo3NX0%3D).", inline=False)
+        embed.add_field(name="**Supporto Tecnico**", value=f"Se hai bisogno di aiuto in gioco, contatta l'assistenza [cliccando qui](https://www.epicgames.com/help/it/contact-us?metadata=eyJoaXN0b3J5TGlua3MiOlt7InVybCI6Ii9mb3J0bml0ZS1jNzUiLCJ0aXRsZSI6IkZvcnRuaXRlIn1dLCJjYXRlZ29yeUlkIjo3NX0%3D).", inline=False)
         #Vars
         mod = get(ctx.guild.roles, id=454262524955852800)
         rvindertech = get(ctx.guild.roles, id=720221658501087312)
         vindertech = get(ctx.guild.roles, id=659513332218331155)
         category = get(ctx.guild.channels, id=683363228931194899)
 	# Ticket Open
-        channel = await ctx.guild.create_text_channel(f"ticket-{user.name}", category=category)
+        channel = await ctx.guild.create_text_channel(f"ticket-{user.name}", category=category, topic=f"Richiesta accettata da <@{str(ctx.author.id)}>")
         await channel.set_permissions(ctx.guild.default_role, read_messages=False, send_messages=False)
         await channel.set_permissions(mod, read_messages=True, send_messages=True, manage_messages=True, embed_links=True, attach_files=True)
         await channel.set_permissions(rvindertech, read_messages=True, send_messages=True, manage_messages=True, embed_links=True, attach_files=True)
