56521273459bd4cfccf09e40cd8a0d3e1c8b7ef9
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-14 20:13:20 +0200

    Update auto-publish.py

--- auto-publish/auto-publish.py
+++ auto-publish/auto-publish.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord import Member, Role, TextChannel, DMChannel
 from discord.ext import commands
@@ -14,11 +16,26 @@ class AutoPublishPlugin(commands.Cog):
     async def on_message(self, message):
         if message.channel.id == 625752242611421214 and message.author.id == 625385492438974502:
             print("AUTOPUBLISH: Message to publish detected")
+            bot_channel = message.guild.get_channel(603955376286728226)
+            embed = discord.Embed(title="**Annullare la Pubblicazione?**", color=discord.Colour.dark_red())
+            embed.description = f"Il [messaggio]({message.jump_url}) sarà presto pubblicato in {message.channel.mention}.\nVuoi forzare l'annullamento dell'operazione?"
+            msg = await bot_channel.send(embed=embed)
+            await msg.add_reaction("❌")
+
+            def check(payload):
+                return payload.user_id != self.bot.user.id and payload.message_id == msg.id and payload.emoji.name == "❌"
+
             try:
-                await message.publish()
-                print("AUTOPUBLISH: Message published")
-            except:
+                await self.bot.wait_for('raw_reaction_add', check=check, timeout=60)
+                await msg.clear_reactions()
+                embed = discord.Embed(title="**Pubblicazione annullata**", color=0x07b43e)
+                await msg.edit(embed=embed)
                 print("AUTOPUBLISH: Message not published")
+            except asyncio.TimeoutError:
+                with suppress(discord.errors.NotFound, discord.errors.HTTPException):
+                    await message.publish()
+                await msg.delete()
+                print("AUTOPUBLISH: Message published")
 
 def setup(bot):
     bot.add_cog(AutoPublishPlugin(bot))
