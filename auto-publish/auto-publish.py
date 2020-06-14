5e3a75e9450cc3a830779378f737e963a3afc494
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-14 19:21:17 +0200

    Update auto-publish.py

--- auto-publish/auto-publish.py
+++ auto-publish/auto-publish.py
@@ -12,11 +12,11 @@ class AutoPublishPlugin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-      if message.channel.id == 721755696118628402:
-        if message.author.id == 625385492438974502:
-            print("AUTOPUBLISH: Message to publish detected")
-            await message.publish()
-            print("AUTOPUBLISH: Message published")
+      #if message.channel.id == 721755696118628402:
+      #  if message.author.id == 625385492438974502:
+      print(f"AUTOPUBLISH: Message to publish detected by {message.author.name}")
+      #      await message.publish()
+      #      print("AUTOPUBLISH: Message published")
 
 def setup(bot):
     bot.add_cog(AutoPublishPlugin(bot))
