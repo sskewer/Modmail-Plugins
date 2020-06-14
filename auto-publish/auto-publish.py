032b3501c15dd068f5e32cacca3a4ba64a1e1458
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-14 19:02:37 +0200

    Update auto-publish.py

--- auto-publish/auto-publish.py
+++ auto-publish/auto-publish.py
@@ -12,23 +12,9 @@ class AutoPublishPlugin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
-        
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        config = await self.db.find_one({"_id": "dm-config"})
-        
-        if config is None:
-            logger.info("User joined, but no DM message was set.")
-            return
-
-        try:
-            message = config["dm-message"]["message"]
-            await member.send(message.replace("{user}", str(member)))
-        except:
-            return
 
     @commands.Cog.listener()
-    async def on_message(message):
+    async def on_message(self, message):
       if message.channel.id == 721755696118628402 and message.author.id == 625385492438974502:
         logger.info("Message to publish detected")
         await message.publish()
