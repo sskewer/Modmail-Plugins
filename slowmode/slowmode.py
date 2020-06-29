39b9349ed8ffc5ee48743813fbf64e74a89f7312
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-29 19:40:35 +0200

    Update slowmode.py

--- slowmode/slowmode.py
+++ slowmode/slowmode.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import Bot, Cog, Context, Greedy, group
 import re
 from core import checks
 from core.models import PermissionLevel
