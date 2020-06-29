6bc4c3a3b1d429ee5c7500d97eed5faad5fd1787
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-29 20:06:44 +0200

    Update slowmode.py

--- slowmode/slowmode.py
+++ slowmode/slowmode.py
@@ -11,10 +11,15 @@ class SlowMode(commands.Cog):
         
     @group(name="slowmode", invoke_without_command=True)
     async def slowmode(self, ctx, time, *, channel: discord.TextChannel):
-        """Impostare la slowmode del canale specificato
+        """Impostare o disattivare la slowmode del canale specificato"""
+        
+    @slowmode.command(name="set")
+    @commands.has_any_role(454262524955852800, 454268394464870401)
+    async def set(self, ctx, *, channel: discord.TextChannel):
+        """
+        Impostare la slowmode del canale specificato
         Non è possibile impostare una slowmode superiore alle 6 ore
         """
-        
         units = {
             "d": 86400,
             "h": 3600,
