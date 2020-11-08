1b0a92d4b4d9447da67d53712dc5f29c56f8013c
Skewer <57370513+sskewer@users.noreply.github.com>
2020-11-08 15:43:01 +0100

    Update faq.py

--- faq/faq.py
+++ faq/faq.py
@@ -1,7 +1,3 @@
-import asyncio
-import datetime
-import copy
-
 import discord
 from discord import File
 from discord.ext import commands
@@ -17,7 +13,47 @@ class faqCommand(commands.Cog):
         
     @commands.guild_only()
     @commands.command(aliases=["domande"])
-    async def faq(self, ctx):
+    async def faq(self, ctx, *, args: str = None):
+    		if ctx.message.mentions == []:
+        		member = None
+        else:
+        		for mention in ctx.message.mentions:
+        			args = args.replace(f"<@!{mention.id}>", "")
+        channel = ctx.guild.get_channel(774706975400919090)
+        messages = await channel.history(limit=50).flatten()
+        titles = []
+        for message in messages:
+        		complete_message = message.content.splitlines( )
+            for line in complete_message:
+            		if "**[" in line:
+                  title = line[line.index("**")+2:line.rindex("**")]
+                	titles.append(title)
+        
+        # Search Function
+        for n, title in enumerate(titles):
+            if args in title:
+                found_message = messages[n]
+                found_title = title
+        
+        # Embed Message
+				embed = discord.Embed(description=f"[`{found_title}`]({found_message.jump_url})", color=discord.Colour.from_rgb(19, 123, 196))
+        if ctx.message.mentions == []:
+        	await ctx.send(embed=embed)
+        else:
+        	epicstaff = ctx.guild.get_role(454262403819896833)
+        	moderatori = ctx.guild.get_role(454262524955852800)
+        	guardiano = ctx.guild.get_role(454268394464870401)
+        	vindertech = ctx.guild.get_role(659513332218331155)
+          if epicstaff in ctx.author.roles or moderatori in ctx.author.roles or guardiano in ctx.author.roles or vindertech in ctx.author.roles:
+          	content = "";
+        		for mention in ctx.message.mentions:
+          		content = content + "<@!" + mention.id + "> "
+        		await ctx.send(content=content, embed=embed)
+          else:
+          	await ctx.send(embed=embed)
+            
+        # Remove Author Message
+        await ctx.message.delete()
         
             
 def setup(bot):
