a524764c587fa707ab11c9225cf0f5d708733840
MettiusHyper <69057048+MettiusHyper@users.noreply.github.com>
2021-10-18 22:22:31 +0200

    refactor powerlevel

--- powerlevel/powerlevel.py
+++ powerlevel/powerlevel.py
@@ -1,25 +1,17 @@
-from discord import Embed, Guild, Member, Role
 from discord.ext import commands
-from discord.ext.commands import Bot, Cog, Context, Greedy, group
 from discord.utils import get
 import re
 
-# Ebic Functions
-levels = {};
-def setup(maxLevel):
-  for i in range(1, maxLevel+1):
-    levels[i] = " [⚡" + str(i) + "]";
- 
 def getNick(nick:str):
   form_nick = re.sub(r'\s+\[⚡\d+\]', '', nick)
   if form_nick:
-    return form_nick;
+    return form_nick
   else:
-    return None;
- 
+    return None
+
 # Setup
-max_level = 138;
-setup(max_level)
+max_level = 138
+allowed_channels = [702576186185875546]
  
 class PowerLevel(commands.Cog):
     def __init__(self, bot):
@@ -28,24 +20,18 @@ class PowerLevel(commands.Cog):
     @commands.command()
     async def powerlevel(self, ctx, *, content:str):
         """Assegnare il livello di Potenza STW al proprio nickname.\n\n**Utilizzo**\n**`/powerlevel <livello>`** - Aggiungere il livello al proprio nickname\n**`/powerlevel reset`** - Rimuovere il livello dal proprio nickname\n\n**Esempi**\n**`/powerlevel 131`** - Nickname [\\⚡131]\n**`/powerlevel reset`** - Nickname\n\n**Accorgimenti**\n- I nickname non possono superare i 32 caratteri.\n- Utilizzare esclusivamente in <#454274882688122880>."""
-        # Vars
-        message = ctx.message;
-        guild   = message.guild;
-        author  = message.author;
-        user_id = author.id;
-        channel = ctx.channel;
-        error   = '**<@' + str(user_id) + '>, per favore inserisci un power level valido.**';
-        # Check
-        if channel.id == 454274882688122880:
+        error = '**<@' + str(ctx.message.author.id) + '>, per favore inserisci un power level valido.**'
+        #channel check
+        if ctx.channel.id in allowed_channels:
           if content.isdigit():
               # Vars
-              index  = int(content);
-              member  = guild.get_member(user_id)
+              index = int(content)
+              member  = ctx.guild.get_member(ctx.message.author.id)
               # New Nickname
               if index > 0 and index <= max_level:
-                  tag = levels[index];
+                  tag = " [⚡" + str(index) + "]"
                   # New Nickname
-                  original_nick = getNick(author.display_name)
+                  original_nick = getNick(ctx.message.author.display_name)
                   await member.edit(nick=original_nick + tag)
                   # Reaction
                   await ctx.message.add_reaction('✅')
@@ -53,11 +39,11 @@ class PowerLevel(commands.Cog):
                   # Send Error Message
                   await ctx.send(error)
                   # Remove Author Message
-                  await message.delete()
+                  await ctx.message.delete()
           elif content == 'reset':
-              member  = guild.get_member(user_id)
+              member = ctx.guild.get_member(ctx.message.author.id)
               # New Nickname
-              original_nick = getNick(author.display_name)
+              original_nick = getNick(ctx.message.author.display_name)
               await member.edit(nick=original_nick)
               # Reaction
               await ctx.message.add_reaction('✅')
@@ -65,10 +51,10 @@ class PowerLevel(commands.Cog):
             # Send Error Message
             await ctx.send(error)
             # Remove Author Message
-            await message.delete()
+            await ctx.message.delete()
         else:
           # Remove Author Message
-          await message.delete()
+          await ctx.message.delete()
        
 def setup(bot):
     bot.add_cog(PowerLevel(bot))
