1e8953ae549bf61fa841dfb564f5a846d905a6b5
Skewer <57370513+sskewer@users.noreply.github.com>
2022-09-30 11:38:52 +0200

    Update easy-reports.py

--- easy-reports/easy-reports.py
+++ easy-reports/easy-reports.py
@@ -1,13 +1,24 @@
+import dislash
+from dislash import *
+
 from datetime import datetime
 
+import re
 import discord
 from discord.ext import commands
 from discord.ext.commands import group
-import re
+
 
 class EasyReports(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = bot.plugin_db.get_partition(self)        
+        
+    def cog_unload(self):
+        self.bot.slash.teardown()
 
 async def setup(bot):
     await bot.add_cog(EasyReports(bot))
+    
+    if not hasattr(bot, "slash"):
+        bot.slash = SlashClient(bot)
