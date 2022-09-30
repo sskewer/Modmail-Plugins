fecf9d2402be3e8741688821e393c0add555c537
Mettius <69057048+MettiusHyper@users.noreply.github.com>
2022-09-30 11:25:41 +0200

    Update reports.py

--- reports/reports.py
+++ reports/reports.py
@@ -1,24 +1,15 @@
 import discord
 from discord.ext import commands
 
-from core import checks
-from core.models import PermissionLevel
-
 from datetime import datetime
 
 
-BaseCog = getattr(commands, "Cog", object)
-
-
-class Report(BaseCog):
+class Report():
     """Un semplice modo per segnalare gli utenti con un comportamento scorretto"""
 
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)
-
-    def cog_unload(self):
-        self.bot.slash.teardown()
 
 
 async def setup(bot):
