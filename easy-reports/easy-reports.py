0c5f4c165a2393e550f43e33f4a26117aa9e35db
Skewer <57370513+sskewer@users.noreply.github.com>
2022-09-30 11:51:49 +0200

    Update easy-reports.py

--- easy-reports/easy-reports.py
+++ easy-reports/easy-reports.py
@@ -1,22 +1,22 @@
-import dislash
-from dislash import *
-
 from datetime import datetime
 
 import re
 import discord
 from discord.ext import commands
 from discord.ext.commands import group
+from discord import app_commands 
 
 
 class EasyReports(commands.Cog):
     def __init__(self, bot):
+        super().__init__(intents = discord.Intents.default())
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)        
+
+    @bot.slash_command(name="test", guild_ids=[454261607799717888])
+    async def test(ctx): 
+        await ctx.respond("You executed the slash command!") 
 
         
 async def setup(bot):
     await bot.add_cog(EasyReports(bot))
-    
-    if not hasattr(bot, "slash"):
-        bot.slash = SlashClient(bot)
