fd14da738ed9f4f5a9c3303d96d05235a4b0f109
Skewer <57370513+sskewer@users.noreply.github.com>
2022-09-30 18:31:03 +0200

    Update easy-reports.py

--- easy-reports/easy-reports.py
+++ easy-reports/easy-reports.py
@@ -13,27 +13,9 @@ class EasyReports(commands.Cog):
         self.bot = bot
         self.db = bot.plugin_db.get_partition(self)  
     
-    @commands.command()
-    async def test(self, ctx):
-        row_of_buttons = ActionRow(
-            Button(
-                style=ButtonStyle.green,
-                label="Green button",
-                custom_id="green"
-            ),
-            Button(
-                style=ButtonStyle.red,
-                label="Red button",
-                custom_id="red"
-            )
-        )
-        msg = await ctx.send("This message has buttons", components=[row_of_buttons])
-        # Wait for a button click
-        def check(inter):
-            return inter.author == ctx.author
-        inter = await msg.wait_for_button_click(check=check)
-        # Process the button click
-        inter.reply(f"Button: {inter.button.label}", type=ResponseType.UpdateMessage)
+    @slash_command(description="Says Hello")
+    async def hello(self, inter):
+        await inter.respond("Hello from cog!")
 
         
 async def setup(bot):
