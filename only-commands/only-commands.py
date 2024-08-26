ac335e3e5afa822d8777a6abc1aa02e6c8f38cab
Matteo <69057048+mettiuss@users.noreply.github.com>
2024-08-26 23:44:43 +0200

    Update only-commands.py

--- only-commands/only-commands.py
+++ only-commands/only-commands.py
@@ -6,11 +6,10 @@ from discord.ext import commands
 class OnlyCommandsPlugin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = bot.plugin_db.get_partition(self)
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == 1277245606645075978 and not message.author.bot and not message.author.guild_permissions.administrator:
+        if message.channel.id == 1277245606645075978:# and not message.author.bot and not message.author.guild_permissions.administrator:
             await message.delete()
 
 def setup(bot):
