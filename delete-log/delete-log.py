c69a1fb7b71193a25ca7576ce2f43f85f082e863
Matteo <69057048+mettiuss@users.noreply.github.com>
2024-08-31 11:01:17 +0200

    Update delete-log.py

--- delete-log/delete-log.py
+++ delete-log/delete-log.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, time
 
 import discord
 from discord.ext import commands
@@ -12,6 +12,7 @@ class DeleteLogPlugin(commands.Cog):
     async def on_raw_message_delete(self, payload):
         guild = await self.bot.fetch_guild(payload.guild_id)
         logChannel = await self.bot.fetch_channel("827832459609374730")
+
         embed = discord.Embed(
             color=discord.Color.red(), title=":wastebasket: Messaggio eliminato", timestamp=datetime.datetime.utcnow()
         )
@@ -19,9 +20,16 @@ class DeleteLogPlugin(commands.Cog):
             text=guild.name, icon_url=guild.icon.url if guild.icon else "https://cdn.discordapp.com/embed/avatars/0.png"
         )
         embed.add_field(name="ID Messaggio", value=f"`{payload.message_id}`", inline=True)
+        deleter = None
         if payload.cached_message:
             if payload.cached_message.author.bot:
                 return
+
+            time.sleep(0.5)
+            async for entry in guild.audit_logs(limit=4, action=discord.AuditLogAction.message_delete):
+                if entry.target.id == payload.cached_message.author.id and entry.extra.channel.id == payload.channel_id:
+                    deleter = entry.user
+
             embed.add_field(
                 name="Utente",
                 value=f"<@{payload.cached_message.author.id}> \n (`{payload.cached_message.author.id}`)",
@@ -35,6 +43,11 @@ class DeleteLogPlugin(commands.Cog):
         embed.add_field(
             name="Messaggio",
             value=payload.cached_message.content if payload.cached_message.content != "" else "`sconosciuto`",
+            inline=False,
+        )
+        embed.add_field(
+            name="Eliminato Da",
+            value=f"<@{deleter}> (`{deleter}`)",
             inline=False,
         )
         await logChannel.send(embed=embed)
