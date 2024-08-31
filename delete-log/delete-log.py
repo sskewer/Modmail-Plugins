df7a91439d6266fbaac9126025fea63cde06a97a
Matteo <69057048+mettiuss@users.noreply.github.com>
2024-08-31 11:53:17 +0200

    Update delete-log.py

--- delete-log/delete-log.py
+++ delete-log/delete-log.py
@@ -1,4 +1,4 @@
-import datetime, asyncio
+import datetime, time, io
 
 import discord
 from discord.ext import commands
@@ -10,10 +10,6 @@ class DeleteLogPlugin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_message_delete(self, payload):
-        if payload.cached_message:
-            if payload.cached_message.author.bot:
-                return
-
         guild = await self.bot.fetch_guild(payload.guild_id)
         logChannel = await self.bot.fetch_channel("827832459609374730")
 
@@ -23,9 +19,17 @@ class DeleteLogPlugin(commands.Cog):
         embed.set_footer(
             text=guild.name, icon_url=guild.icon.url if guild.icon else "https://cdn.discordapp.com/embed/avatars/0.png"
         )
-        
         embed.add_field(name="ID Messaggio", value=f"`{payload.message_id}`", inline=True)
-        if payload.cached_message: 
+        deleter = None
+        if payload.cached_message:
+            if payload.cached_message.author.bot:
+                return
+
+            time.sleep(0.5)
+            async for entry in guild.audit_logs(limit=4, action=discord.AuditLogAction.message_delete):
+                if entry.target.id == payload.cached_message.author.id and entry.extra.channel.id == payload.channel_id:
+                    deleter = entry.user.id
+
             embed.add_field(
                 name="Utente",
                 value=f"<@{payload.cached_message.author.id}> \n (`{payload.cached_message.author.id}`)",
@@ -36,27 +40,21 @@ class DeleteLogPlugin(commands.Cog):
             value=f"<#{payload.channel_id}> \n (`{payload.channel_id}`)",
             inline=True,
         )
+        if len(payload.cached_message.content) < 1024:
+            embed.add_field(
+                name="Messaggio",
+                value=payload.cached_message.content if len(payload.cached_message.content) != 0 else "`sconosciuto`",
+                inline=False,
+            )
         embed.add_field(
-            name="Messaggio",
-            value=payload.cached_message.content if payload.cached_message.content != "" else "`sconosciuto`",
+            name="Eliminato Da",
+            value=f"<@{deleter}> (`{deleter}`)",
             inline=False,
         )
-
-        if payload.cached_message:
-            deleter = None
-
-            await asyncio.sleep(0.5)
-            async for entry in guild.audit_logs(limit=4, action=discord.AuditLogAction.message_delete):
-                if entry.target.id == payload.cached_message.author.id and entry.extra.channel.id == payload.channel_id:
-                    deleter = entry.user.id
-
-            if deleter:
-                embed.add_field(
-                    name="Eliminato Da",
-                    value=f"<@{deleter}> (`{deleter}`)",
-                    inline=False,
-                )
-        await logChannel.send(embed=embed)
+        if len(payload.cached_message.content) < 1024:
+            await logChannel.send(embed=embed)
+        else:
+            await logChannel.send(embed=embed, file=discord.File(io.StringIO(text_content), filename="content.txt"))
 
 
 async def setup(bot):
